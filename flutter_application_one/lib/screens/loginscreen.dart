54bdac93467c61ec3b02756858b2e3121f6797b0
AnishaVineeth <anishavineeth01@gmail.com>
2021-10-06 10:55:33 +0530

    firebase auth update

--- flutter_application_one/lib/screens/loginscreen.dart
+++ flutter_application_one/lib/screens/loginscreen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'homescreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -10,112 +13,142 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool isHiddenPassword = true;
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final GlobalKey<FormState> _key = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
+        User? user = FirebaseAuth.instance.currentUser;
+
     return Material(
       //  type: MaterialType.transparency,
-      child: Container(
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.all(40.0),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const Padding(
-              padding: const EdgeInsets.only(top: 10.0),
-              child: Text(
-                'Company Name',
-                style: TextStyle(color: Colors.grey),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
+      child: Form(
+        key: _key,
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Padding(
+                padding: const EdgeInsets.only(top: 10.0),
                 child: Text(
-                  "Sign In",
-                  style: TextStyle(fontSize: 30, color: Colors.black),
+                  'Company Name',
+                  style: TextStyle(color: Colors.grey),
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Hi there! Nice to see you again.",
-                  style: TextStyle(fontSize: 14, color: Colors.grey),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Sign In",
+                    style: TextStyle(fontSize: 30, color: Colors.black),
+                  ),
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                decoration: InputDecoration(
-                  // border: OutlineInputBorder(),
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.red),
-                  hintText: 'Enter valid mail id',
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Hi there! Nice to see you again.",
+                    style: TextStyle(fontSize: 14, color: Colors.grey),
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                obscureText: isHiddenPassword,
-                decoration: InputDecoration(
-                    // border: OutlineInputBorder(),
-                    labelText: 'Password',
+               Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  validator: validateEmail,
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
                     labelStyle: TextStyle(color: Colors.red),
-                    hintText: 'Enter your password',
-                    suffixIcon: InkWell(
-                        onTap: _togglePassword,
-                        child: Icon(
-                          isHiddenPassword
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                        ))),
-              ),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.redAccent,
-                    padding:
-                        EdgeInsets.symmetric(horizontal: 120, vertical: 12)),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => HomeScreen()),
-                  );
-                },
-                child: Text('Sign In')),
-            const SizedBox(
-              height: 110,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    'Forgot Password?',
+                    hintText: 'Enter valid mail id',
                   ),
-                  SizedBox(
-                    width: 80,
-                  ),
-                  Align(
-                      alignment: Alignment.centerRight,
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            color: Colors.red, fontWeight: FontWeight.bold),
-                      ))
-                ],
+                ),
               ),
-            )
-          ]),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  validator: validatePassword,
+                  controller: passwordController,
+                  obscureText: isHiddenPassword,
+                  decoration: InputDecoration(
+                      // border: OutlineInputBorder(),
+                      labelText: 'Password',
+                      labelStyle: TextStyle(color: Colors.red),
+                      hintText: 'Enter your password',
+                      suffixIcon: InkWell(
+                          onTap: _togglePassword,
+                          child: Icon(
+                            isHiddenPassword
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ))),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.redAccent,
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 120, vertical: 12)),
+                  onPressed: () async {
+                    if (_key.currentState!.validate()) {
+                      await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                              email: emailController.text,
+                              password: passwordController.text);
+                      setState(() {});
+                    }
+                    await FirebaseAuth.instance.signInWithEmailAndPassword(
+                        email: emailController.text,
+                        password: passwordController.text);
+                    setState(() {});
+
+                  },
+                  child: Text('Sign In')),
+              const SizedBox(
+                height: 110,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:  [
+                    Text(
+                      'Forgot Password?',
+                    ),
+                    SizedBox(
+                      width: 80,
+                    ),
+                    Align(
+                        alignment: Alignment.centerRight,
+                        child: InkWell(
+                          onTap: ()async{
+                            if (_key.currentState!.validate()) {
+                              await FirebaseAuth.instance
+                                  .createUserWithEmailAndPassword(
+                                      email: emailController.text,
+                                      password: passwordController.text);
+                              setState(() {});
+                            }
+                          },
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                                color: Colors.red, fontWeight: FontWeight.bold),
+                          ),
+                        ))
+                  ],
+                ),
+              )
+            ]),
+          ),
         ),
       ),
     );
@@ -129,4 +162,14 @@ class _LoginScreenState extends State<LoginScreen> {
     }
     setState(() {});
   }
+}
+String? validateEmail(String? formEmail) {
+  if (formEmail == null || formEmail.isEmpty) return 'Email is required';
+  return null;
+}
+
+String? validatePassword(String? formPassword) {
+  if (formPassword == null || formPassword.isEmpty)
+    return 'Password is required';
+  return null;
 }
